000e634d3da9da55d08f000f14de820e7d7a98b2
birklep <birkle.p@googlemail.com>
2025-11-19 00:38:59 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("rt37-cache").then(cache => {
+    caches.open("rt37-cache-v2").then(cache => {
       return cache.addAll([
         "./index.html",
         "./manifest.json"
@@ -21,3 +21,4 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+
