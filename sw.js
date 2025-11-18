4416c1033c9d876811fc6413c6c761c48ddd4aa6
birklep <birkle.p@googlemail.com>
2025-11-19 00:16:38 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("rt37-cache").then(cache => {
+      return cache.addAll([
+        "./index.html",
+        "./manifest.json"
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
