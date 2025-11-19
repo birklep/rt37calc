5427a777d7e30c81eec7882ddc8aa511f3726140
birklep <birkle.p@googlemail.com>
2025-11-19 14:52:43 +0100

    Update sw.js
    Update auf neue Format, wo Chache sauber gesetzt wird und alte Versionen gelöscht werden.

--- sw.js
+++ sw.js
@@ -1,17 +1,31 @@
+const CACHE_NAME = "rt37-cache-20251119-1452";
+
+const OFFLINE_URLS = [
+  "/rt37calc/",
+  "/rt37calc/index.html",
+  "/rt37calc/manifest.json",
+  "/rt37calc/icon-192.png",
+  "/rt37calc/icon-512.png"
+];
+
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("rt37-cache-20251119").then(cache => {
-      return cache.addAll([
-        "./index.html",
-        "./manifest.json"
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(OFFLINE_URLS))
   );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))   // ALTE CACHES WEG
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
@@ -21,5 +35,3 @@ self.addEventListener("fetch", event => {
     })
   );
 });
-
-
