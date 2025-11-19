34af1c8657caf40d08661147f9b4a93439d31e66
birklep <birkle.p@googlemail.com>
2025-11-19 15:54:36 +0100

    Update sw.js
    sw zurücksetzten das sich irgendwie nichts mehr aktualisiert

--- sw.js
+++ sw.js
@@ -1,31 +1,17 @@
-const CACHE_NAME = "rt37-cache-20251119-1452";
-
-const OFFLINE_URLS = [
-  "/rt37calc/",
-  "/rt37calc/index.html",
-  "/rt37calc/manifest.json",
-  "/rt37calc/icon-192.png",
-  "/rt37calc/icon-512.png"
-];
-
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(OFFLINE_URLS))
+    caches.open("rt37-cache-20251119").then(cache => {
+      return cache.addAll([
+        "./index.html",
+        "./manifest.json"
+      ]);
+    })
   );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))   // ALTE CACHES WEG
-      )
-    )
-  );
-  self.clients.claim();
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", event => {
@@ -35,3 +21,4 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+
